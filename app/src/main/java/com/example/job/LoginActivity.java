efdb9dad72bb880cec0287dfecb2858c87ab65ea
Your Name <2358931408@qq.com>
2023-11-25 00:25:10 +0800

    init

---
+++ app/src/main/java/com/example/job/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.job;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+    Button button_login;
+    Button button_register;
+    EditText text_account;
+    EditText text_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_login);
+        button_login = findViewById(R.id.login_login);
+        button_register = findViewById(R.id.login_register);;
+        text_account = findViewById(R.id.login_account);
+        text_password = findViewById(R.id.login_password);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = text_account.getText().toString();;
+                String password = text_password.getText().toString();
+                if (Objects.equals(name, "123") && Objects.equals(password, "456")) { // TODO: condition
+                    Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "用户不存在或密码错误", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
