6012b2d1a132cc18eb05a28128d1ac535a734821
Your Name <2358931408@qq.com>
2023-12-01 22:00:27 +0800

    顶真

--- app/src/main/java/com/example/job/MainActivity.java
+++ app/src/main/java/com/example/job/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.job;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +21,16 @@ import com.example.job.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    public static final String STOP_MUSIC_ACTION = "com.example.job.STOP_MUSIC";
+
+    private MediaPlayer mediaPlayer;
+
+    private BroadcastReceiver stopMusicReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            stopMusic();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
+        registerReceiver(stopMusicReceiver, new IntentFilter(STOP_MUSIC_ACTION));
 
+        // 设置点击事件监听器
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void stopMusic() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        // 在 Activity 销毁时取消注册广播接收器
+        unregisterReceiver(stopMusicReceiver);
+
+        super.onDestroy();
+    }
 }
