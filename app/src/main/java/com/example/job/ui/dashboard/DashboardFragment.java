c5932cc0dc6fb5621912f885152027a9090ec75d
1 <21371202@buaa.edu.cn>
2023-12-07 20:58:11 +0800

    genshin impact update

--- app/src/main/java/com/example/job/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/job/ui/dashboard/DashboardFragment.java
@@ -1,17 +1,24 @@
 package com.example.job.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.job.Job.JobAdapter;
+import com.example.job.Job.JobItem;
 import com.example.job.Module;
 import com.example.job.Position;
+import com.example.job.R;
+import com.example.job.WebViewActivity;
 import com.example.job.databinding.FragmentDashboardBinding;
 
 import java.util.ArrayList;
@@ -27,10 +34,18 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        // final TextView textView = binding.textDashboard;
-        // dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listView = view.findViewById(R.id.listViewFavorites);
+        ArrayList<JobItem> jobItems = new ArrayList<>();
+        JobAdapter jobAdapter = new JobAdapter(getContext(), R.layout.job_item, jobItems);
+        listView.setAdapter(jobAdapter);
+
+        jobItems.add(new JobItem("Playing Genshin Impact", "Li Tang", "Ding Zhen", "5000/month", "www.baidu.com"));
+        // TODO 此处更改以适应收藏夹
     }
 
     @Override
