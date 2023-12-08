62778c6484fdb903dc5f7380b84a51480d0eacab
Your Name <2358931408@qq.com>
2023-12-08 18:57:27 +0800

    href

--- app/src/main/java/com/example/job/JobSearchService.java
+++ app/src/main/java/com/example/job/JobSearchService.java
@@ -109,7 +109,8 @@ public class JobSearchService extends Service {
                     String company = jsonObject.getString("company");
                     String hrname = jsonObject.getString("hrname");
                     String href = jsonObject.getString("href");
-                    JobsAll.getAll().add(new JobItem(title,company,hrname,salary,"https://www.zhipin.com/"+href));
+                    String url= webView.getUrl();
+                    JobsAll.getAll().add(new JobItem(title,company,hrname,salary,"https://www.zhipin.com"+href));
                 }
             } catch (JSONException e) {
                 throw new RuntimeException(e);
